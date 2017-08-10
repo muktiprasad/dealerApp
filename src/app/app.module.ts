cc58b3eae662b8ae8c346ee38afe8a21b7630d0e
muktiprasad <mukti.pattnaik@gmail.com>
2017-08-10 17:36:49 -0500

    Added the filter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { CreateComponent }   from './create.component';
 import { PageNotFoundComponent }     from './not.found.component';
 import { AppComponent }  from './app.component';
 import { FormsModule }   from '@angular/forms';
-import { EditComponent } from './edit.component'
+import { EditComponent } from './edit.component';
+import {columnPipe,rowPipe,searchPipe} from './pipe';
 const appRoutes: Routes = [
   { path: 'create', component: CreateComponent },
   { path: 'edit', component: EditComponent },
@@ -18,7 +19,7 @@ const appRoutes: Routes = [
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     ) ],
-  declarations: [ AppComponent,CreateComponent,PageNotFoundComponent,EditComponent ],
+  declarations: [ AppComponent,CreateComponent,PageNotFoundComponent,EditComponent,columnPipe,rowPipe,searchPipe ],
   
   bootstrap:    [ AppComponent ]
 })
